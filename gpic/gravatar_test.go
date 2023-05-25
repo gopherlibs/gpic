bc5a40f43b9fb97eecaddcd9c792a8a5e2ad8cae
Ricardo N Feliciano <FelicianoTech@gmail.com>
2023-05-25 17:21:40 -0400

    Add GitHub support

--- gpic/gravatar_test.go
+++ gpic/gravatar_test.go
@@ -24,18 +24,18 @@ func TestImageURL(t *testing.T) {
 		emailHash    string
 		defaultImage string
 		rating       rating
-		size         uint16
+		size         int16
 		expectedURL  string
 	}{
-		{[]string{"info@chayev.com","Ricardo@Feliciano.Tech"}, "f6d625c59c19ea57fe2c3d7968a56f29", "", 0, 0, "https://www.gravatar.com/avatar/f6d625c59c19ea57fe2c3d7968a56f29.jpg?rating=g&size=80"},
-		{[]string{"Ricardo@Feliciano.Tech"}, "f6d625c59c19ea57fe2c3d7968a56f29", "", 0, 0, "https://www.gravatar.com/avatar/f6d625c59c19ea57fe2c3d7968a56f29.jpg?rating=g&size=80"},
+		{[]string{"info@chayev.com", "Ricardo@Feliciano.Tech"}, "f6d625c59c19ea57fe2c3d7968a56f29", "", 0, 80, "https://www.gravatar.com/avatar/f6d625c59c19ea57fe2c3d7968a56f29.jpg?rating=g&size=80"},
+		{[]string{"Ricardo@Feliciano.Tech"}, "f6d625c59c19ea57fe2c3d7968a56f29", "", 0, 0, "https://www.gravatar.com/avatar/f6d625c59c19ea57fe2c3d7968a56f29.jpg?rating=g"},
 		{[]string{"Ricardo@Feliciano.Tech"}, "f6d625c59c19ea57fe2c3d7968a56f29", "", 0, 50, "https://www.gravatar.com/avatar/f6d625c59c19ea57fe2c3d7968a56f29.jpg?rating=g&size=50"},
 		{[]string{"Ricardo@Feliciano.Tech"}, "f6d625c59c19ea57fe2c3d7968a56f29", "", RatingR, 50, "https://www.gravatar.com/avatar/f6d625c59c19ea57fe2c3d7968a56f29.jpg?rating=r&size=50"},
-		{[]string{"Ricardo@Feliciano.Tech"}, "f6d625c59c19ea57fe2c3d7968a56f29", "", RatingR, 3000, "https://www.gravatar.com/avatar/f6d625c59c19ea57fe2c3d7968a56f29.jpg?rating=r&size=80"},
-		{[]string{"Ricardo@Feliciano.Tech"}, "f6d625c59c19ea57fe2c3d7968a56f29", "identicon", 0, 0, "https://www.gravatar.com/avatar/f6d625c59c19ea57fe2c3d7968a56f29.jpg?default=identicon&rating=g&size=80"},
+		{[]string{"Ricardo@Feliciano.Tech"}, "f6d625c59c19ea57fe2c3d7968a56f29", "", RatingR, 3000, "https://www.gravatar.com/avatar/f6d625c59c19ea57fe2c3d7968a56f29.jpg?rating=r&size=2048"},
+		{[]string{"Ricardo@Feliciano.Tech"}, "f6d625c59c19ea57fe2c3d7968a56f29", "identicon", 0, -1, "https://www.gravatar.com/avatar/f6d625c59c19ea57fe2c3d7968a56f29.jpg?default=identicon&rating=g&size=2048"},
 		{[]string{"Ricardo@Feliciano.Tech"}, "f6d625c59c19ea57fe2c3d7968a56f29", "retro", RatingPG, 2000, "https://www.gravatar.com/avatar/f6d625c59c19ea57fe2c3d7968a56f29.jpg?default=retro&rating=pg&size=2000"},
 		{[]string{"info@chayev.com"}, "328576744df0329b287e83fb6257ebb5", "", 0, 50, "https://www.gravatar.com/avatar/328576744df0329b287e83fb6257ebb5.jpg?rating=g&size=50"},
-		{[]string{"Ricardo@Feliciano.Tech","info@chayev.com"}, "f6d625c59c19ea57fe2c3d7968a56f29", "", RatingR, 50, "https://www.gravatar.com/avatar/f6d625c59c19ea57fe2c3d7968a56f29.jpg?d=404&rating=r&size=50"},
+		{[]string{"Ricardo@Feliciano.Tech", "info@chayev.com"}, "f6d625c59c19ea57fe2c3d7968a56f29", "", RatingR, 50, "https://www.gravatar.com/avatar/f6d625c59c19ea57fe2c3d7968a56f29.jpg?d=404&rating=r&size=50"},
 	}
 
 	for idx, sample := range samples {
@@ -48,9 +48,9 @@ func TestImageURL(t *testing.T) {
 		i.defaultImage = sample.defaultImage
 		i.SetSize(sample.size)
 		i.rating = sample.rating
-		
+
 		if url, _ := i.URL(); url.String() != sample.expectedURL {
-			t.Errorf(" got url: %q; expected: %q; #: %d\n", url, sample.expectedURL, idx)
+			t.Errorf(" got url: %q; expected: %q; #: %d\n", url, sample.expectedURL, idx+1)
 		}
 	}
 }
