1759b7438f24b815392a41b95e40f912bf0c9f3b
Michael Ibragimchayev <michael@chayev.com>
2023-05-30 00:03:33 -0400

    Update tests to work properly. (#27)

--- gpic/gpic_test.go
+++ gpic/gpic_test.go
@@ -30,28 +30,28 @@ func TestNewAvatar(t *testing.T) {
 		// sample 6
 		{[]string{"info@chayev.com", "Ricardo@Feliciano.Tech"}, 256, "https://www.gravatar.com/avatar/f6d625c59c19ea57fe2c3d7968a56f29.jpg?rating=g&size=256", true},
 		// sample 7
-		{[]string{"Ricardo@Feliciano.Tech", "info@chayev.com"}, 256, "https://www.gravatar.com/avatar/f6d625c59c19ea57fe2c3d7968a56f29.jpg?rating=g&size=256", true},
+		{[]string{"Ricardo@Feliciano.Tech", "info@chayev.com"}, 256, "https://www.gravatar.com/avatar/f6d625c59c19ea57fe2c3d7968a56f29.jpg?d=404&rating=g&size=256", true},
 		// GitHub username
 		// sample 8
-		{[]string{"gh:felicianotech"}, 1000, "https://avatars.githubusercontent.com/u/6017470?v=4&size=1000", true},
+		{[]string{"gh:felicianotech"}, 1000, "https://avatars.githubusercontent.com/u/6017470?size=460&v=4", true},
 		// GitHub ID
 		// sample 9
 		{[]string{"ghi:6017470"}, 0, "https://avatars.githubusercontent.com/u/6017470?v=4", true},
 		// Fake email, GitHub username
 		// sample 10
-		{[]string{"info@chayev.com", "gh:chayev"}, 32, "https://avatars.githubusercontent.com/u/18604081?v=4&size=32", true},
+		{[]string{"info@chayev.com", "gh:chayev"}, 32, "https://avatars.githubusercontent.com/u/18604081?size=32&v=4", true},
 		// GitHub username, fake email.... GitHub doesn't have defaults so is always prioritized
 		// sample 11
-		{[]string{"gh:chayev", "info@chayev.com"}, 32, "https://avatars.githubusercontent.com/u/18604081?v=4&size=32", true},
+		{[]string{"gh:chayev", "info@chayev.com"}, 32, "https://avatars.githubusercontent.com/u/18604081?size=32&v=4", true},
 		// Uses a special fake token designed just for tests
 		// sample 12
-		{[]string{"ghp_test-working-token"}, 32, "https://avatars.githubusercontent.com/u/6017470?v=4&size=32", true},
+		{[]string{"ghp_test-working-token"}, 32, "https://avatars.githubusercontent.com/u/6017470?size=32&v=4", true},
 		// Uses a special fake token designed just for tests
 		// sample 13
 		{[]string{"ghp_test-broken-token"}, 0, "", false},
 		// Uses a special fake token designed just for tests, first a bad one, then a good one
 		// sample 14
-		{[]string{"ghp_test-broken-token", "ghp_test-working-token"}, 0, "", true},
+		{[]string{"ghp_test-broken-token", "ghp_test-working-token"}, 0, "https://avatars.githubusercontent.com/u/6017470?v=4", true},
 		// GitLab username
 		// sample 15
 		{[]string{"gl:chayev"}, 0, "https://gitlab.com/uploads/-/system/user/avatar/5945977/avatar.png", true},
@@ -63,7 +63,7 @@ func TestNewAvatar(t *testing.T) {
 		{[]string{"glpat-test-working-token"}, 100, "https://gitlab.com/uploads/-/system/user/avatar/5945977/avatar.png?width=200", true},
 		// GitLab broken token and GitLab username
 		// sample 18
-		{[]string{"glpat-test-broken-token","gl:felicianotech"}, 90, "https://gitlab.com/uploads/-/system/user/avatar/283008/avatar.png?width=96", true},
+		{[]string{"glpat-test-broken-token","gl:FelicianoTech"}, 90, "https://gitlab.com/uploads/-/system/user/avatar/283008/avatar.png?width=96", true},
 		// GitLab token - Uses a special fake token designed just for tests
 		// sample 19
 		{[]string{"glpat-test-broken-token"}, 0, "", false},
@@ -75,14 +75,14 @@ func TestNewAvatar(t *testing.T) {
 		if err != nil && sample.pass {
 			t.Errorf("Failed creating new avatar, when it should have passed. Pass is: %t #: %d\n err: %v\n\n", sample.pass, idx+1, err)
 			continue
-		} else {
+		} else if err != nil && !sample.pass {
 			continue
 		}
 
 		avatar.SetSize(sample.size)
 
 		if url, _ := avatar.URL(); url.String() != sample.expectedURL {
-			t.Errorf(" got url: %q; expected: %q; #: %d\n", url, sample.expectedURL, idx+1)
+			t.Errorf("\ngot url: %q;\nexpected: %q;\n#: %d\n\n\n", url, sample.expectedURL, idx+1)
 		}
 	}
 }
